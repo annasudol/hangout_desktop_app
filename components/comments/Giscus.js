06d69eb2160f8acc79c8352780b08266149614e0
Anna Sudol <sudolann@gmail.com>
2023-01-22 19:26:56 +0000

    removed theme

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -1,17 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { useTheme } from 'next-themes'
 
 import siteMetadata from '@/data/siteMetadata'
 
 const Giscus = () => {
   const [enableLoadComments, setEnabledLoadComments] = useState(true)
-  const { theme, resolvedTheme } = useTheme()
-  const commentsTheme =
-    siteMetadata.comment.giscusConfig.themeURL === ''
-      ? theme === 'dark' || resolvedTheme === 'dark'
-        ? siteMetadata.comment.giscusConfig.darkTheme
-        : siteMetadata.comment.giscusConfig.theme
-      : siteMetadata.comment.giscusConfig.themeURL
 
   const COMMENTS_ID = 'comments-container'
 
@@ -41,7 +33,6 @@ const Giscus = () => {
     script.setAttribute('data-emit-metadata', metadata)
     script.setAttribute('data-input-position', inputPosition)
     script.setAttribute('data-lang', lang)
-    script.setAttribute('data-theme', commentsTheme)
     script.setAttribute('crossorigin', 'anonymous')
     script.async = true
 
@@ -52,7 +43,7 @@ const Giscus = () => {
       const comments = document.getElementById(COMMENTS_ID)
       if (comments) comments.innerHTML = ''
     }
-  }, [commentsTheme])
+  }, [])
 
   // Reload on theme change
   useEffect(() => {
--- components/comments/Utterances.js
+++ components/comments/Utterances.js
@@ -1,16 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { useTheme } from 'next-themes'
 
 import siteMetadata from '@/data/siteMetadata'
 
 const Utterances = () => {
   const [enableLoadComments, setEnabledLoadComments] = useState(true)
-  const { theme, resolvedTheme } = useTheme()
-  const commentsTheme =
-    theme === 'dark' || resolvedTheme === 'dark'
-      ? siteMetadata.comment.utterancesConfig.darkTheme
-      : siteMetadata.comment.utterancesConfig.theme
-
   const COMMENTS_ID = 'comments-container'
 
   const LoadComments = useCallback(() => {
@@ -20,7 +13,6 @@ const Utterances = () => {
     script.setAttribute('repo', siteMetadata.comment.utterancesConfig.repo)
     script.setAttribute('issue-term', siteMetadata.comment.utterancesConfig.issueTerm)
     script.setAttribute('label', siteMetadata.comment.utterancesConfig.label)
-    script.setAttribute('theme', commentsTheme)
     script.setAttribute('crossorigin', 'anonymous')
     script.async = true
 
@@ -31,7 +23,7 @@ const Utterances = () => {
       const comments = document.getElementById(COMMENTS_ID)
       if (comments) comments.innerHTML = ''
     }
-  }, [commentsTheme])
+  }, [])
 
   // Reload on theme change
   useEffect(() => {
