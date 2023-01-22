afd9b4f26e1cd59b6e19d165cfbf9d3d0760765f
Anna Sudol <sudolann@gmail.com>
2023-01-22 19:52:28 +0000

    sitemap

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,9 +18,6 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
   analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports plausible, simpleAnalytics, umami or googleAnalytics
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
