00da7b108c53bb0b4547dff3358d1d1e66600caf
Anna Sudol <sudolann@gmail.com>
2023-01-22 19:21:02 +0000

    app updates

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,11 +1,11 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'platform for socialize',
   author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  headerTitle: 'Hangout',
+  description: 'App for creating events and seeking new connections',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://hangout.chat',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
